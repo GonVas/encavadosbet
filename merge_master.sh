7c470c3a93ae40dd60d5b3f3d7650d7c9165d9bd
Goncalo Vasconcelos Cunha Miranda Moreno <gonvas@goncalo.moreno97@gmail.com>
2018-09-29 19:57:15 +0100

    Trying to fix bottle 500 error issue

---
+++ merge_master.sh
@@ -0,0 +1,7 @@
+git push origin encavadosbet_dev
+git checkout master
+git merge encavadosbet_dev
+git push origin master
+git branch -d encavadosbet_dev
+git checkout master
+git checkout -b encavadosbet_dev
