91469073e29ce2fcb44c8107d363064c9e4b8d6a
Goncalo Vasconcelos Cunha Miranda Moreno <gonvas@goncalo.moreno97@gmail.com>
2018-09-30 05:44:21 +0100

    Added Login System

--- encavadosbet.py
+++ encavadosbet.py
@@ -11,12 +11,6 @@ import sqlite3
 
 app = bottle.default_app()
 TEMPLATE_PATH.insert(0, os.path.abspath(os.path.dirname(__file__)))
-
-candidates = [{'name':'OCO', 'file':'sample', 'matriculas':'4', 'curso':'Inphormatica', 'votos':100},
-{'name':'Togapi', 'file':'sample', 'matriculas':'8', 'curso':'Metal Bio', 'votos':30},
-{'name':'Rodriguez', 'file':'sample', 'matriculas':'10', 'curso':'Electro', 'votos':130} ]
-
-
 cand_template = Template(' <section class=\"card\"> <header> </header> <img src=\"$image.jpg\" width=\"200\" height=\"200\"> <ul> <li>&nbsp;$name</li> <li>&nbsp;$curso</li> <li>&nbsp;$matriculas MATRICULAS</li> </ul> <button> Apostar </button> <progress value=\"$votos\" max=\"$totalvotos\"> </section>')
 
 abs_app_dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -24,12 +18,22 @@ abs_app_dir_path = os.path.dirname(os.path.realpath(__file__))
 bd = sqlite3.connect('final.db')
 bd_cursor = bd.cursor()
 
-@route('/')
-def check_app():
+no_login_bar = ''' <ul class="topbar" >
+  <li><a href="#login" onclick="login_click()">Mostar Carta de Caloiro &nbsp;</a></li>
+  <li><a href="#register"> Registar Carta de Caloiro</a></li>
 
-    print(abs_app_dir_path)
+  <li style="float:right"><a class="active" href="#about">Engenharia</a></li>
+</ul> '''
+
+def check_login(username, password):
+    if username == "Oco" and password == "Oco":
+        return True
+    else:
+        return False
+
+def get_candidates():
+
     final_string = ''
-
     cand = bd_cursor.execute('''SELECT NAME, MATRICULAS, CURSO, IMAGE FROM voter WHERE active != 0 ''').fetchall()
     total_votes = int(bd_cursor.execute("SELECT Sum(cnt) from (select Count(token) as cnt from voter inner join vote ON vote.what_candidate = voter.token GROUP by Name)").fetchone()[0])
     print("Total Number of votes: " + str(total_votes))
@@ -43,7 +47,6 @@ def check_app():
         new_candidate['image'] = ca[3]
         new_candidate['votos'] = bd_cursor.execute("SELECT Count(token) from voter inner join vote ON vote.what_candidate = voter.token WHERE NAME = ?", (ca[0],)).fetchone()[0]
         new_candidate['totalvotos'] = total_votes
-
         new_candidates.append(new_candidate)
 
 
@@ -52,12 +55,58 @@ def check_app():
         complete_card = cand_template.substitute(candidate)
         final_string += complete_card
 
-    info = {'title': "Apostas Encavados", 'cards':final_string}
-    return template('page.tpl', info)
+    return final_string
+
+def get_userbar(username):
+    user = bd_cursor.execute('''SELECT NAME, MATRICULAS, CURSO, IMAGE FROM voter WHERE NAME == ? ''', (username, )).fetchone()
+    login_bar = Template(''' <ul class="topbar" >
+  <li> <div> <img src="$image.jpg" alt="thumbnail" width="60" height="60" > </div> </li>
+  <li><a> $name </a></li>
+  <li><a> $matriculas inscricoes </a></li>
+  <li style="float:right"><a class="active" href="#about">$curso</a></li>
+</ul>''').substitute(image=user[3],name = user[0], matriculas = user[1], curso = user[2])
+    return login_bar
+
+@route('/')
+def check_app():
+
+    username = request.get_cookie("account", secret='some-secret-key')
+    if username:
+        print("User " + str(username) + " entered") 
+        return template('page.tpl', {'topbar':get_userbar(str(username)), 'cards':get_candidates()})
+    else:
+        print("UnKnown user entered")
+        return template('page.tpl', {'topbar':no_login_bar, 'cards':get_candidates()})
+
+    
+
+
+@route('/login', method='POST')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    if check_login(username, password):
+        response.set_cookie("account", username, secret='some-secret-key')
+        return template('page.tpl', {'topbar':get_userbar(username), 'cards':get_candidates()})
+        #return template("<p>Welcome {{name}}! You are now logged in.</p>", name=username)
+    else:
+        return template('page.tpl', {'topbar':no_login_bar, 'cards':get_candidates()})
+        #return "<p>Login failed.</p>"
+
+@route('/restricted')
+def restricted_area():
+    username = request.get_cookie("account", secret='some-secret-key')
+    if username:
+        return template("Hello {{name}}. Welcome back.", name=username)
+    else:
+        return "You are not logged in. Access denied."
+
 
 @route('/<filename:path>')
 def send_static(filename):
     return static_file(filename, root=abs_app_dir_path)
+
+
 
 
 if __name__ == '__main__':
--- page.tpl
+++ page.tpl
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-  <title>{{title}}</title>
+  <title>Apostas Encavados</title>
   <link href="https://fonts.googleapis.com/css?family=Romanesco" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="/start_page.css">
   <link rel="shortcut icon" type='image/x-icon' href="/favicon.ico">
@@ -19,9 +19,13 @@
 <body>
 
 <!-- Main content -->
+
+
+{{!topbar}}
+
     <img src="dura.jpg" alt="Dura Praxis Sed Praxis">
 
-    <h1>{{title}}</h1>
+    <h1>Apostas Encavados</h1>
 
     <p id="desc">
 
@@ -43,16 +47,37 @@
 </audio>
 <button type="button" onclick="aud_play_pause()">Musica</button>
 
+
+ <!-- Trigger/Open The Modal -->
+<button id="myBtn">Open Modal</button>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content" >
+    <span class="close" onclick="modal_block()">&times;</span>
+     <form action="/login" method="post">
+            username: <input name="username" type="text" />
+            &nbsp;password: <input name="password" type="password" />
+            <input value="Login" type="submit" />
+    </form>
+  </div>
+
+</div> 
+
+
     <div class="spooky">
-      <img src="doot.gif" alt="skeleton" onmouseover="PlaySound('doot')">
+      <img src="doot.gif" class="floatl" alt="skeleton" onmouseover="PlaySound('doot')">
       <!--img src="tenor.gif" alt="tenor"-->
-      <img src="tpose.gif" alt="tpose" width="220" height="220" onmouseover="PlaySound('nigra')">
-      <img src="move.gif" alt="move">
+      <img src="tpose.gif" class="floatr" alt="tpose" width="220" height="220" onmouseover="PlaySound('nigra')">
+      <!--img src="move.gif" alt="move"-->
+      <h2>⭐️ &nbsp;Candidatos&nbsp; ⭐️ </h2>
     </div>
 
 
 
-<h2>⭐️ &nbsp;Candidatos&nbsp; ⭐️ </h2>
+<h2> &nbsp; </h2>
 
 
 <div class="cards">
@@ -74,6 +99,42 @@ function PlaySound(soundobj) {
     var thissound=document.getElementById(soundobj);
     thissound.play();
 }
+
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+//var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+function login_click(){
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+a.onclick = function() {
+    modal.style.display = "none";
+}
+
+function modal_block(){
+modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+} 
+
 
 </script>  
 
